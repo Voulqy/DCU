84051c5e98f8ab2c1a06fb9198bf24d31c406cb9
Voulqy <47714156+Voulqy@users.noreply.github.com>
2021-06-06 01:43:31 -0500

    dark mode added

--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -1,8 +1,26 @@
-document.getElementById('change-theme-btn').addEventListener('click', function () {
-    let darkThemeEnabled = document.body.classList.toggle('dark-theme');
-    localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
-});
+const STORAGE_KEY = 'theme';
+const THEME_ATTRIBUTE = 'data-theme';
+const THEME_BTN_CLASS = '.js-theme-btn';
 
-if (JSON.parse(localStorage.getItem('dark-theme-enabled'))) {
-    document.body.classList.add('dark-theme');
+function setTheme() {
+	// Are we using the "light" or "dark" theme?
+	let theme = localStorage.getItem(STORAGE_KEY); // Either "light" or "dark"
+
+	// Set the data attribute to the theme
+	document.body.setAttribute(THEME_ATTRIBUTE, theme);
 }
+
+function handleThemeBtnClick() {
+	// figure what do we need to change the theme to?
+	const currentTheme = document.body.getAttribute(THEME_ATTRIBUTE);
+
+	const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+	
+	localStorage.setItem(STORAGE_KEY, newTheme);
+
+	setTheme();
+}
+
+document.querySelector(THEME_BTN_CLASS).addEventListener('click', handleThemeBtnClick);
+
+setTheme();
