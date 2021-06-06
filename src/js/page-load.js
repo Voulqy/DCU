83303314061642bac2d0056b388160b17a20489b
Voulqy <47714156+Voulqy@users.noreply.github.com>
2021-06-06 05:28:03 -0500

    testing jquery

---
+++ src/js/page-load.js
@@ -0,0 +1,11 @@
+// load home page
+$(document).ready(function(){
+    // $("#main").load("../src/html/sample.html");
+
+        $(".menu #home").addClass('active');
+});
+
+
+// $(document).ready(function(){
+
+// });
--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -1,47 +1,30 @@
-// const STORAGE_KEY = 'theme';
-// const THEME_ATTRIBUTE = 'data-theme';
-// const THEME_BTN_CLASS = '.js-theme-btn';
-
-// function setTheme() {
-// 	// Are we using the "light" or "dark" theme?
-// 	let theme = localStorage.getItem(STORAGE_KEY); // Either "light" or "dark"
-
-// 	// Set the data attribute to the theme
-// 	document.body.setAttribute(THEME_ATTRIBUTE, theme);
+// function $(el) {
+//     return document.querySelector(el);
 // }
 
-// function handleThemeBtnClick() {
-// 	// figure what do we need to change the theme to?
-// 	const currentTheme = document.body.getAttribute(THEME_ATTRIBUTE);
-
-// 	const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-	
-// 	localStorage.setItem(STORAGE_KEY, newTheme);
-
-// 	setTheme();
-// }
-
-// document.querySelector(THEME_BTN_CLASS).addEventListener('click', handleThemeBtnClick);
-
-// setTheme();
-
-
-function $(el){
-    return document.querySelector(el);
-  }
-  
-  let container = $('.container');
-  let toggle = $('#toggle-theme'); 
-  
-  toggle.addEventListener('click', () => {
-    let list = container.classList;
-    
-    if(list.contains('default')){
-      list.replace('default','light');
-      toggle.textContent = 'Dark';
-    }else{
-      list.replace('light','default');
-      toggle.textContent = 'Light'; 
+$('.toggle').click(function(){
+    var $this = $(".default");
+    if (!$this.hasClass('light')) {
+        $this.addClass('light')
+    } else {
+        $this.removeClass('light')
     }
-    
-  })
+})
+
+
+// js
+// let container = $('.container');
+// let toggle = $('#toggle-theme');
+
+// toggle.addEventListener('click', () => {
+//     let list = container.classList;
+
+//     if (list.contains('default')) {
+//         list.replace('default', 'light');
+//         toggle.textContent = 'Dark';
+//     } else {
+//         list.replace('light', 'default');
+//         toggle.textContent = 'Light';
+//     }
+
+// })
