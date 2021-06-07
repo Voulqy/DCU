ed36be784dfb62ba3bc9834f8dee8073c76213e5
Voulqy <47714156+Voulqy@users.noreply.github.com>
2021-06-06 20:02:44 -0500

    Update #37 - Pages now scroll to the top when loaded

--- src/js/page-load.js
+++ src/js/page-load.js
@@ -14,11 +14,13 @@ $(document).ready(function () {
     // Cookies.set('currPage', 'index');
     $('.logo').click(function(){
         $('#main').load("index.html")
+        $(window).scrollTop(0);
     });
 
     $('#artists').click(function(){
         $('li.active').removeClass('active');
         $('#main').load("src/html/artists.html")
+        $(window).scrollTop(0);
         $('title').text("Artists");
         var $this = $(this);
         if (!$this.hasClass('active')) {
@@ -30,6 +32,7 @@ $(document).ready(function () {
     $('#music').click(function(){
         $('li.active').removeClass('active');
         $('#main').load("src/html/discography.html")
+        $(window).scrollTop(0);
         $('title').text("Discography");
         var $this = $(this);
         if (!$this.hasClass('active')) {
@@ -43,16 +46,19 @@ $(document).ready(function () {
 $('.discography-card').click(function(){
     var albumId = this.id;
     $("#main").load("src/html/albums/" + albumId + ".html");
+    $(window).scrollTop(0);
 });
 
 // Load individual lyrics pages
 $('.song').click(function(){
     var songId = this.id;
     $("#main").load("src/html/songs/" + songId + ".html");
+    $(window).scrollTop(0);
 });
 
 // Go back a to the album page
 $('.go-back').click(function(){
     var albumId = this.id;
     $("#main").load("src/html/albums/" + albumId + ".html");
+    $(window).scrollTop(0);
 });
